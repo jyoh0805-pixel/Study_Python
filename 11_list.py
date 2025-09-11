257f1685f7077250f86a8d87ae3ecfc29c32d7ca
오준영 <ohjunyoung@email.com>
2025-09-11 12:49:28 +0900

    list 정렬 맟 석재

--- 11_list.py
+++ 11_list.py
@@ -25,6 +25,8 @@ a.insert(1,'X')
 
 print(f'a 의 길이 : {len(a)}')
 print(f'a : {a}')
+
+
 # a의 2번방에 있는 값
 print(f'a[2] : {a[2]}')
 # a의 가장 마지막 방에 있는 값
@@ -33,3 +35,23 @@ print(f'a의 마지막 방의 값 : {a[3]}')
 print(f'a의 마지막 방의 값 : {a[len(a)-1]}')
 # 파이썬에서 사용되는 방식, 0 보다 뒤로가면 맨 뒤로 이동된다는 개념
 print(f'a의 마지막 방의 값 : {a[-1]}')
+
+# 리스트 정렬(sort)
+shop_list.sort() # 오름차순
+print(f'shop_list : {shop_list}')
+
+shop_list.sort(reverse=True) # 내림차순
+print(f'shop_list : {shop_list}')
+
+# sorted 는 원본의 리스트를 정렬한 값을 새로운 리스트로 반환
+new_list = sorted(shop_list)
+print(f'new_list : {new_list}')
+
+# a의 2번 인덱스에 c를 넣는다.
+# insert 와 다른점 해당 인덱스의 값을 지우고 그자리에 들어간다는 것이다.
+a[2] = 'c'
+print(f'a : {a}')
+
+# list 삭제
+del a[1]
+print(f'a : {a}')
