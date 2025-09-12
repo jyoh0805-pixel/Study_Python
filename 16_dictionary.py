8fdca98822d40699af2044abc7413fbf27400505
오준영 <ohjunyoung@email.com>
2025-09-12 11:11:16 +0900

    dic 꺼내기2

--- 16_dictionary.py
+++ 16_dictionary.py
@@ -26,3 +26,10 @@ print(a)
 # 사전의 특정 요소를 꺼내보자(사용법은 List와 비슷하다.)
 print(dic2['name'])
 print(dic2['friends'])
+
+# get 메서드를 활용해서도 가져올수 있다.
+print(dic2.get('phone'))
+
+# 특정 키가 없는경우 None이 아닌 대체 내용으로 반환할수 있다.
+print(dic2.get('nick','해당 내용이 없음'))
+
