679782b977cb402cf9ae45a6286204f65b548713
오준영 <ohjunyoung@email.com>
2025-09-15 09:53:09 +0900

    함수 인자값

--- 21_function_args.py
+++ 21_function_args.py
@@ -1,5 +1,7 @@
-def plus(num):
+# 인자값으로 아무것도 들어오지 않았을경우 에러를 방지하기 위해 기본값 설정이 가능
+def plus(num=0):
     result = num + 5
     return result
+
 print(plus(5))  # 10
 print(plus())   # plus() missing 1 required positional argument: 'num'
