830702443026ba98e2722957e8e33d7163cfb802
김다미 <kimdamee@gmail.com>
2025-09-16 16:46:14 +0900

    다중 예외 처리방식

---
+++ advance/13_test.py
@@ -0,0 +1,9 @@
+
+while True:
+    try:
+        text = input('값을 넣으면 숫자로 변환 됩니다.')
+        num = int(text)
+        print(f'당신이 입력한 값 :{num} 이 숫자로 변환 되었습니다.')
+        break
+    except ValueError:
+        print(f'{text} 는 숫자가 아닙니다.');
