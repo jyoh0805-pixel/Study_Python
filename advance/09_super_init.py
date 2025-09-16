df29e8bc8272e31213d42ad051f4e039c185470f
김다미 <kimdamee@gmail.com>
2025-09-16 12:36:18 +0900

    생략된 부모 생성자

---
+++ advance/09_super_init.py
@@ -0,0 +1,10 @@
+class parent():
+    def __init__(self):
+        print("부모 생성자 실행!")
+
+class Child(parent):
+    def __init__(self):
+        super().__init__() # 생략된 부모 생성자
+        print("자식 생성자")
+
+ch = Child()
