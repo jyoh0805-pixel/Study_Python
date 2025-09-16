866a0fdfe84c03049c94a3a5f6251d684b9c9d8c
김다미 <kimdamee@gmail.com>
2025-09-16 12:04:34 +0900

    점퍼만들기

---
+++ advance/08_inherit.py
@@ -0,0 +1,18 @@
+class Runner:
+    def run(self):
+        pass
+
+    def sprint(self):
+        pass
+
+
+class Jumper:
+    def jump(self):
+        pass
+
+    def high_jump(self):
+        pass
+
+
+class Person(Jumper, Runner):
+    pass
