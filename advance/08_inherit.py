56afa048d23bb4592957e4009f0a8631d1beb414
김다미 <kimdamee@gmail.com>
2025-09-16 12:12:35 +0900

    상속예시

--- advance/08_inherit.py
+++ advance/08_inherit.py
@@ -1,18 +1,29 @@
 class Runner:
     def run(self):
-        pass
+        print(f'달린다.')
 
     def sprint(self):
-        pass
+        print(f'전력질주를 한다.')
 
 
 class Jumper:
     def jump(self):
-        pass
+        print(f'점프를 한다.')
 
     def high_jump(self):
-        pass
+        print(f'높이 점프를 한다.')
 
 
-class Person(Jumper, Runner):
-    pass
+class Person(Jumper, Runner): # Jumper와 Runner를 상속 받았다.
+    def walk(self):
+        print(f'걷는다.')
+
+# walk() 함수를 사용하기 위해 Person 클래스를 객체화 한다.
+p = Person()
+p.walk()
+
+# 상속받은 함수들을 내것처럼(p 객체화로부터) 사용한다.
+p.jump()
+p.high_jump()
+p.run()
+p.sprint()
